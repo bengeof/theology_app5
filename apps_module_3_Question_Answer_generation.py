493202e0aea6751c92301ed4659d59698e8ca5a7
bengeof <65452898+bengeof@users.noreply.github.com>
2024-12-22 16:54:15 +0530

    apps_module_3_Question_Answer_generation.py

--- apps_module_3_Question_Answer_generation.py
+++ apps_module_3_Question_Answer_generation.py
@@ -5,9 +5,9 @@ import streamlit as st
 #https://stackoverflow.com/questions/18885175/read-a-zipped-file-as-a-pandas-dataframe 
 
 import pandas as pd
-url='https://drive.google.com/file/d/1DlpbMAqIB50aJVyMRES_J_CnxYdRXH-p/view?usp=sharing'
+url='https://drive.google.com/file/d/1PHOOkOHVD2IX3gyxtHiyn5ZdFrJGdYFX/view?usp=sharing'
 
-df = pd.read_csv('https://drive.usercontent.google.com/download?id={}&export=download&authuser=0&confirm=t'.format(url.split('/')[-2]), sep='##', names=['text', 'key', 'period' , 'title'])
+df = pd.read_csv('https://drive.usercontent.google.com/download?id={}&export=download&authuser=0&confirm=t'.format(url.split('/')[-2]), sep='\t', names=['text', 'key', 'period' , 'title'])
 print(df)
 import os
 
