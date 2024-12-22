0727d2f4a5edbfc1d5f9aa28375877ce79923a12
bengeof <65452898+bengeof@users.noreply.github.com>
2024-12-22 09:35:22 +0530

    apps_module_3_Question_Answer_generation.py

--- apps_module_3_Question_Answer_generation.py
+++ apps_module_3_Question_Answer_generation.py
@@ -144,19 +144,6 @@ if generate_button:
             loader = TextLoader('modul1_inter2.txt')
             documents = loader.load()
             
-            import textwrap
-            
-            def wrap_text_preserve_newlines(text, width=110):
-                # Split the input text into lines based on newline characters
-                lines = text.split('\n')
-            
-                # Wrap each line print(wrap_text_preserve_newlines(str(documents[0])))
-                wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
-            
-                # Join the wrapped lines back together using newline characters
-                wrapped_text = '\n'.join(wrapped_lines)
-            
-                return wrapped_text
             
             from langchain.text_splitter import CharacterTextSplitter
             text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -233,19 +220,6 @@ if generate_button:
             loader = TextLoader('modul1_inter1.txt')
             documents = loader.load()
             
-            import textwrap
-            
-            def wrap_text_preserve_newlines(text, width=110):
-                # Split the input text into lines based on newline characters
-                lines = text.split('\n')
-            
-                # Wrap each line print(wrap_text_preserve_newlines(str(documents[0])))
-                wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
-            
-                # Join the wrapped lines back together using newline characters
-                wrapped_text = '\n'.join(wrapped_lines)
-            
-                return wrapped_text
             
             from langchain.text_splitter import CharacterTextSplitter
             text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
