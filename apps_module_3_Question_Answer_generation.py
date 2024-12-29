1b76899b75e597923c73637d149fadc03efb1f7a
bengeof <65452898+bengeof@users.noreply.github.com>
2024-12-29 16:43:23 +0530

    apps_module_3_Question_Answer_generation.py

--- apps_module_3_Question_Answer_generation.py
+++ apps_module_3_Question_Answer_generation.py
@@ -33,7 +33,7 @@ st.set_page_config(
 )
 
 # Add a title and a description
-st.title("Module_2_User_query")
+st.title("Module_3_Question_Answer_form_Summary")
 st.write(
     "This application generates question answer based on the topic choosen on the author's text of interest."
 )
